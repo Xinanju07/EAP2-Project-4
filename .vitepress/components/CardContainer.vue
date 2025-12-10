f842b3099ed1bcad5aa0197f5e631a5f3f9ce19e
Xinanju07 <wsyfreedom@gmail.com>
2025-12-10 01:56:15 -0500

    Update CardContainer.vue

--- .vitepress/components/CardContainer.vue
+++ .vitepress/components/CardContainer.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="container flex flex-wrap justify-center mx-auto gap-x-4">
     <ArticleCard
-      v-for="article in articles"
+      v-for="article in sortedArticles"
+      :key="article.link"
       :title="article.frontmatter.title"
       :author="article.frontmatter.author"
       :date="article.date.string"
       :desc="article.frontmatter.description"
       :tags="formatTags(article.frontmatter.tags)"
       :link="article.link"
-    >
-    </ArticleCard>
+    />
   </div>
 </template>
 
@@ -17,7 +17,16 @@
 import ArticleCard from "./ArticleCard.vue";
 import { data as articles } from "../data/articles.data";
 
+// -------------------------------
+//  Automatic date sorting (ASC)
+//  最旧 → 最新
+// -------------------------------
+const sortedArticles = articles.sort((a, b) => {
+  return new Date(a.date.string).getTime() - new Date(b.date.string).getTime();
+});
+
 function formatTags(tags: string) {
   return tags ? tags.split("/") : [];
 }
 </script>
+
